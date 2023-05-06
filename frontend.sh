17e4d4109b92f651352660c6e7d745a5dcc32e3c
Anass <shaikanass304@gmail.com>
2023-05-06 20:06:30 +0530

    using fuctions for color the output dynamically

--- frontend.sh
+++ frontend.sh
@@ -2,26 +2,30 @@ code_dir=$(pwd)
 log_file=/tmp/rorboshop.log
 rm-f ${log_file}
 
-echo -e "\e[35mInstalling nginx\e[0m"
+print_head(){
+  echo e- "\e[35m$1"
+}
+
+"Installing nginx"
 yum install nginx -y
 
-echo -e "\e[35mRemoving old content\e[0m"
+print_head"Removing old content"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[35mDownloading frontend content\e[0m"
+print_head"Downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>{log_file}
 
-echo -e "\e[35mExtracting Downloaded frontend content\e[0m"
+print_head"Extracting Downloaded frontend content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>{log_file}
 
-echo -e "\e[35mCopying nginx config for Roboshop\e[0m"
+print_head"Copying nginx config for Roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>{log_file}
 
-echo -e "\e[35mEnabling nginx\e[0m"
+print_head"Enabling nginx"
 systemctl enable nginx
 
-echo -e "\e[35mRestart nginx\e[0m"
+print_head"Restart nginx"
 systemctl restart nginx
 
 # Roboshop config is not copied
