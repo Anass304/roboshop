88681216ce72af48ada0b7885c4c2e39ed7ef7be
Anass <shaikanass304@gmail.com>
2023-05-06 19:48:03 +0530

    copying of roboshop config file path issue has been resolved using code_dir=$(pwd), and we call that in cp${code_dir}/configs  ... and i have implmented the redirectors for output and log those in log_file

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,7 @@
+code_dir=$(pwd)
+log_file=/tmp/rorboshop.log
+rm-f ${log_file}
+
 echo -e "\e[35mInstalling nginx\e[0m"
 yum install nginx -y
 
@@ -5,14 +9,14 @@ echo -e "\e[35mRemoving old content\e[0m"
 rm -rf /usr/share/nginx/html/*
 
 echo -e "\e[35mDownloading frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>{log_file}
 
 echo -e "\e[35mExtracting Downloaded frontend content\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>{log_file}
 
 echo -e "\e[35mCopying nginx config for Roboshop\e[0m"
-cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>{log_file}
 
 echo -e "\e[35mEnabling nginx\e[0m"
 systemctl enable nginx
