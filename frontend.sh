9f61687db24040b6029e259c2e03a82a0cc63e96
Anass <shaikanass304@gmail.com>
2023-05-06 20:09:29 +0530

    using fuctions for color the output dynamically

--- frontend.sh
+++ frontend.sh
@@ -9,23 +9,23 @@ print_head(){
 "Installing nginx"
 yum install nginx -y
 
-print_head"Removing old content"
+print_head "Removing old content"
 rm -rf /usr/share/nginx/html/*
 
-print_head"Downloading frontend content"
+print_head "Downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>{log_file}
 
-print_head"Extracting Downloaded frontend content"
+print_head "Extracting Downloaded frontend content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>{log_file}
 
-print_head"Copying nginx config for Roboshop"
+print_head "Copying nginx config for Roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>{log_file}
 
-print_head"Enabling nginx"
+print_head "Enabling nginx"
 systemctl enable nginx
 
-print_head"Restart nginx"
+print_head "Restart nginx"
 systemctl restart nginx
 
 # Roboshop config is not copied
