7b389aae060ec45a51d65ea8914c246cf7154a01
Anass <shaikanass304@gmail.com>
2023-05-07 11:57:54 +0530

    code dry

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,44 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-rm-rf /app/*
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp configs/catalogue.service etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+source common.sh
+print_head "configure NodeJS Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.anassdevops.online </app/schema/catalogue.js
+print_head "Install NodeJs "
+yum install nodejs -y &>>{log_file}
+
+print_head "Create Roboshop user "
+useradd roboshop &>>{log_file}
+
+print_head "Create Application Directory"
+mkdir /app &>>{log_file}
+
+print_head "Create old content "
+rm-rf /app/* &>>{log_file}
+
+print_head "Downloading App content"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{log_file}
+cd /app
+
+print_head "Extracting App Content "
+unzip /tmp/catalogue.zip &>>{log_file}
+cd /app
+
+print_head "Installing NodeJs dependencyes"
+npm install &>>{log_file}
+
+print_head "Copy SystemD service File "
+cp configs/catalogue.service etc/systemd/system/catalogue.service &>>{log_file}
+print_head "Reload systemd"
+systemctl daemon-reload
+print_head "Enabling catalouge service"
+systemctl enable catalogue
+print_head "Start Catalouge Services"
+systemctl restart catalogue
+
+print_head "Copy MongoDB Repo File"
+cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>{log_file}
+
+print_head "Install Mongo Client "
+yum install mongodb-org-shell -y &>>{log_file}
+
+print_head "Load Schema "
+mongo --host mongodb.anassdevops.online </app/schema/catalogue.js &>>{log_file}
