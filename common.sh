5a84e8201c7808a416d57d32a7aac10456b09070
Anass <shaikanass304@gmail.com>
2023-05-07 20:07:09 +0530

    .

--- common.sh
+++ common.sh
@@ -16,56 +16,57 @@ if [ $1? -eq 0 ]; then
     }
     
     NODEJS(){
+
       print_head "configure NodeJS Repo"
       curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-      
+
       #status_check $?
       print_head "Install NodeJs "
       yum install nodejs -y &>>{log_file}
       status_check $?
-      
+
       print_head "Create Roboshop ${component}"
       if roboshop &>>{log_ile}
       if [ $? -ne 0 ]; then
         ${component}add roboshop &>>{log_file}
         fi
       status_check $?
-      
+
       print_head "Create Application Directory"
       if [ ! -d /app ]; then
       mkdir /app &>>{log_file}
       fi
       status_check $?
-      
+
       print_head "Create old content "
       rm-rf /app/* &>>{log_file}
       status_check $?
-      
+
       print_head "Downloading App content"
-      curl -o /tmp/${component.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>{log_file}
+      curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>{log_file}
       status_check $?
       cd /app
-      
+
       print_head "Extracting App Content "
       unzip /tmp/${component}.zip &>>{log_file}
-      
+
       status_check $?
       cd /app
-      
+
       print_head "Installing NodeJs dependencyes"
       npm install &>>{log_file}
       status_check $?
       print_head "Copy SystemD service File "
       cp ${code_dir}/configs/${component}.service etc/systemd/system/${component}.service &>>{log_file}
-      
+
       status_check $?
       print_head "Reload systemd"
       systemctl daemon-reload
-      
+
       status_check $?
       print_head "Enabling ${component}service"
       systemctl enable usser
-      
+
       status_check $?
       print_head "Start ${component}Services"
       systemctl restart ${component
@@ -79,4 +80,6 @@ if [ $1? -eq 0 ]; then
       print_head "Load Schema "
       mongo --host mongodb.anassdevops.online </app/schema/${component}.js &>>{log_file}
       status_check $?
-    }
+
+
+}
