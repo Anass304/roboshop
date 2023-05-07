253f3bddb3e8394248701e9e92bc04794c3362c3
Anass <shaikanass304@gmail.com>
2023-05-07 19:12:52 +0530

    redis

---
+++ redis.sh
@@ -0,0 +1,27 @@
+source comman.sh
+print_head "Installing redis repo files "
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_file}
+
+status_check $?
+print_head "Enable 6.2 redis repo "
+dnf module enable redis:remi-6.2 -y &>>${log_file}
+status_check $?
+
+print_head "Installing redis "
+yum install redis -y &>>{log_file}
+status_check $?
+
+print_head "Update redis listne adress "
+sde -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+status_check $?
+
+print_head "Enabling redis  "
+systemctl enable redis
+
+status_check $?
+
+print_head "Starting redis "
+systemctl start redis
+status_check $?
+
+
