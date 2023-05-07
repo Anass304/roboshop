9395bf8594e17342342598197a739c3699cc8654
Anass <shaikanass304@gmail.com>
2023-05-08 00:25:20 +0530

    .

--- shipping.sh
+++ shipping.sh
@@ -3,6 +3,8 @@ source common.sh
 myswl_root_password=$1
 if [ ${mysql_root_password} == "mysql"]; then
   echo -e "\e[MYSQL  ROOT assword argument\e[m"
+  exit -1
+  fi
 component=shipping
 schema_type="mysql"
 java
