819eb986a5e706c84ad347b22a6149c8873d8074
Anass <shaikanass304@gmail.com>
2023-05-07 12:52:03 +0530

    updated ip adress

--- mongodb.sh
+++ mongodb.sh
@@ -3,9 +3,13 @@ print_head "setup mongodb repository "
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 print_head "installimg mongodb"
 yum install mongodb-org -y
+
+print_head "Update MongoDB Listen address"
+
+sed -i -e 's/127.0.0.1/0.0.0/' /etc/mongodb.conf
 print_head "enabling mongodb"
 systemctl enable mongod
 print_head "start mongodb service"
-systemctl start mongod
+systemctl restart mongod
 
-# update /etc/mongodb.conf file from 123.0.0.1 with 0.0.0.0
+# update /etc/mongodb.conf file from 127.0.0.1 with 0.0.0.0
